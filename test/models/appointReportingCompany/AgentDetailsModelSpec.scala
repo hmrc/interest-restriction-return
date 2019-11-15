9f6de42be3dd8278fd469d34df09c3b97aefa7ef
Ladon Jackson <31283663+LadonJackson@users.noreply.github.com>
2019-11-15 09:34:09 +0000

    [IRR-105]: added models

---
+++ test/models/appointReportingCompany/AgentDetailsModelSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.appointReportingCompany
+
+import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.Json
+
+class AgentDetailsModelSpec extends WordSpec with Matchers {
+
+  "AgentDetailsModel" must {
+
+    "correctly write to json" in {
+
+
+      val expectedValue = Json.obj(
+        "agentActingOnBehalfOfCompany" -> true,
+        "agentName" -> "name"
+      )
+
+      val actualValue = Json.toJson(AgentDetailsModel(true, Some("name")))
+
+      actualValue shouldBe expectedValue
+    }
+
+    "correctly read from Json" in {
+
+      val expectedValue = AgentDetailsModel(true, Some("name"))
+
+      val actualValue = Json.obj(
+        "agentActingOnBehalfOfCompany" -> true,
+        "agentName" -> "name"
+      ).as[AgentDetailsModel]
+
+      actualValue shouldBe expectedValue
+    }
+  }
+}
