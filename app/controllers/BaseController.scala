bc91d9e5e2ccede9068f12abf679dd1ae0f12d60
“mywyau” <47248104+mywyau@users.noreply.github.com>
2019-12-10 15:37:18 +0000

    Json Error Msgs

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -16,12 +16,33 @@
 
 package controllers
 
+import play.api.libs.json._
+import play.api.mvc.{Request, Result}
 import uk.gov.hmrc.play.bootstrap.controller.BackendBaseController
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
-import scala.concurrent.ExecutionContext
+
+case class ValidationErrorResponse(field:JsPath, errors:Seq[JsonValidationError])
+
+object ValidationErrorResponse {
+  implicit val writes:Writes[ValidationErrorResponse] = Writes{models =>
+    Json.obj("field" -> models.field.toString(), "errors" -> models.errors.map(_.message))
+  }
+}
 
 trait BaseController extends BackendBaseController {
 
   implicit val ec: ExecutionContext = controllerComponents.executionContext
 
+  override def withJsonBody[T](
+                                 f: (T) => Future[Result])(implicit request: Request[JsValue], m: Manifest[T], reads: Reads[T]): Future[Result] =
+    Try(request.body.validate[T]) match {
+      case Success(JsSuccess(payload, _)) => f(payload)
+      case Success(JsError(errs)) =>
+            Future.successful(BadRequest(Json.toJson(errs.map{
+              case (field, errors) => ValidationErrorResponse(field, errors)
+            })))
+      case Failure(e) => Future.successful(BadRequest(s"Could not parse body due to ${e.getMessage}"))
+    }
 }
