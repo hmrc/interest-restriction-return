0d53a402b7f7575d7ae807c50695927cda635cd4
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2019-12-19 15:32:34 +0000

    [CIR-260] Change to handle numeric value for date by rejecting it

--- app/models/AccountingPeriodModel.scala
+++ app/models/AccountingPeriodModel.scala
@@ -18,13 +18,25 @@ package models
 
 import java.time.LocalDate
 
-import play.api.libs.json.Json
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json.{Json, _}
 import validation.AccountingPeriodValidator
+
+import scala.util.Try // Combinator syntax
 
 case class AccountingPeriodModel(startDate: LocalDate, endDate: LocalDate) extends AccountingPeriodValidator {
   override val accountingPeriodModel: AccountingPeriodModel = this
 }
 
 object AccountingPeriodModel {
-  implicit val format = Json.format[AccountingPeriodModel]
+
+  private def readDate(field: String): Reads[LocalDate] = (__ \ field).read[String]
+    .filter(JsonValidationError("Date must be in ISO Date format YYYY-MM-DD"))(_.matches(raw"(\d{4})-(\d{2})-(\d{2})"))
+    .filter(JsonValidationError("Date must be a valid date"))(str => Try(LocalDate.parse(str)).isSuccess)
+    .map(LocalDate.parse)
+
+  implicit val reads = (readDate("startDate") and readDate("endDate"))(AccountingPeriodModel.apply _)
+
+  implicit val writes = Json.writes[AccountingPeriodModel]
 }
