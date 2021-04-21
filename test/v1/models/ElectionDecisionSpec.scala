dc34a6f83074eb5061e9fc141241ecd6fad64be0
DevAndyroo <andy@andyroo.info>
2021-04-21 10:58:10 +0100

    Add election decision test

---
+++ test/v1/models/ElectionDecisionSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models
+
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.TryValues._
+import play.api.libs.json.{JsonValidationError, JsPath, JsResultException, JsString}
+
+import scala.util.Try
+
+class ElectionDecisionSpec extends WordSpec with Matchers {
+  "ElectionDecision" should {
+    "error when an incorrect value is entered" in {
+      val json = JsString("Something incorrect")
+      val result = Try(json.as[ElectionDecision])
+
+      result.failure.exception shouldEqual JsResultException(Seq((JsPath, Seq(JsonValidationError(s"Unknown ElectionDecision")))))
+    }
+  }
+}
