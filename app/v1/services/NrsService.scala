32b415ca27af98d4d030ad787b8b30edafb0dbc8
burns238 <13621631+burns238@users.noreply.github.com>
2021-05-21 14:00:38 +0100

    CIR-1681 - Update NRS metadata

--- app/v1/services/NrsService.scala
+++ app/v1/services/NrsService.scala
@@ -24,7 +24,6 @@ import v1.models.requests.IdentifierRequest
 import javax.inject.{Inject, Singleton}
 import com.google.common.io.BaseEncoding.base64
 import play.api.libs.json.{JsObject, JsString, JsValue}
-import play.api.mvc.AnyContentAsXml
 import v1.connectors.NrsConnector
 import uk.gov.hmrc.http._
 import play.api.Logging
@@ -37,27 +36,27 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class NrsService @Inject()(nrsConnector: NrsConnector)(implicit ec: ExecutionContext) extends HttpErrorFunctions with Logging {
 
-  private val conversationIdKey = "conversationId"
-  private val applicationXml = "application/xml"
-  private val businessIdValue = "cds"
-  private val notableEventValue = "cds-declaration"
+  private val searchKey = "searchKey" //TODO: Update once NRS have updated the Calling Service Registry with a value for us https://confluence.tools.tax.service.gov.uk/display/NR/Calling+Service+Registry
+  private val applicationJson = "application/json"
+  private val businessIdValue = "irr" //TODO: Update once NRS have updated the Calling Service Registry with a value for us https://confluence.tools.tax.service.gov.uk/display/NR/Calling+Service+Registry
+  private val notableEventValue = "irr-submission" //TODO: Update once NRS have updated the Calling Service Registry with a value for us https://confluence.tools.tax.service.gov.uk/display/NR/Calling+Service+Registry
   private val AuthorizationHeader = "Authorization"
 
-  def send[A](implicit authRequest: IdentifierRequest[A], hc: HeaderCarrier): Future[NrsResponse] = {
+  def send[A](implicit identifierRequest: IdentifierRequest[A]): Future[NrsResponse] = {
 
-    val payloadAsString = authRequest.request.body.asInstanceOf[AnyContentAsXml].xml.toString
+    val payloadAsString = identifierRequest.request.body.toString
 
-    val conversationId = "vpr.conversationId.toString"
+    val searchValue = "" //TODO: Update once NRS have updated the Calling Service Registry with a value for us https://confluence.tools.tax.service.gov.uk/display/NR/Calling+Service+Registry
 
     val nrsMetadata = new NrsMetadata(businessId = businessIdValue,
       notableEvent = notableEventValue,
-      payloadContentType = applicationXml,
+      payloadContentType = applicationJson,
       payloadSha256Checksum = sha256Hash(payloadAsString), // This should come from the end user NOT us
       userSubmissionTimestamp = nowUtc().toString,
-      userAuthToken = authRequest.request.headers.get(AuthorizationHeader).getOrElse(""),
-      identityData = authRequest.nrsRetrievalData,
-      headerData = new JsObject(authRequest.request.headers.toMap.map(x => x._1 -> JsString(x._2 mkString ","))),
-      searchKeys = JsObject(Map[String, JsValue](conversationIdKey -> JsString(conversationId)))
+      userAuthToken = identifierRequest.request.headers.get(AuthorizationHeader).getOrElse(""),
+      identityData = identifierRequest.nrsRetrievalData,
+      headerData = new JsObject(identifierRequest.request.headers.toMap.map(x => x._1 -> JsString(x._2 mkString ","))),
+      searchKeys = JsObject(Map[String, JsValue](searchKey -> JsString(searchValue)))
     )
 
     val nrsPayload: NrsPayload = NrsPayload(base64().encode(payloadAsString.getBytes(UTF_8)), nrsMetadata)
