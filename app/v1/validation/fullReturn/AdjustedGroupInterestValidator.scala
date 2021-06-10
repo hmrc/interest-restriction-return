c7dac5018717cb2c9951b29263eb4f42ebfbc53d
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2021-06-10 11:54:08 +0100

    CIR-1740 Ran formater

--- app/v1/validation/fullReturn/AdjustedGroupInterestValidator.scala
+++ app/v1/validation/fullReturn/AdjustedGroupInterestValidator.scala
@@ -72,13 +72,13 @@ trait AdjustedGroupInterestValidator extends BaseValidation {
   }
 
   private def groupRatioCalculationDoesntMatch(qngie: BigDecimal, groupEBITDA: BigDecimal, groupRatio: BigDecimal): Boolean = {
-      val hasDecimals = groupRatio.toString.contains(".")
-      val decimalPlaces = if (hasDecimals) {
-        groupRatio.toString.length - groupRatio.toString.indexOf(".") - 1
-      } else {
-        0
-      }
-      groupRatio != ((qngie / groupEBITDA) * 100).min(100).setScale(decimalPlaces, RoundingMode.DOWN)
+    val hasDecimals = groupRatio.toString.contains(".")
+    val decimalPlaces = if (hasDecimals) {
+      groupRatio.toString.length - groupRatio.toString.indexOf(".") - 1
+    } else {
+      0
+    }
+    groupRatio != ((qngie / groupEBITDA) * 100).min(100).setScale(decimalPlaces, RoundingMode.DOWN)
   }
 
   def validate(implicit path: JsPath): ValidationResult[AdjustedGroupInterestModel] =
@@ -86,53 +86,53 @@ trait AdjustedGroupInterestValidator extends BaseValidation {
       validateQngie,
       validateGroupEBITDA,
       validateGroupRatioCalculation).mapN((_, _, _, _) => adjustedGroupInterestModel)
-  }
+}
 
-  case class QngieDecimalError(qngie: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "QNGIE_DECIMAL"
-    val errorMessage: String = "QNGIE must be to 2 decimal places or less"
-    val path: JsPath = topPath \ "qngie"
-    val value = Some(Json.toJson(qngie))
-  }
+case class QngieDecimalError(qngie: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "QNGIE_DECIMAL"
+  val errorMessage: String = "QNGIE must be to 2 decimal places or less"
+  val path: JsPath = topPath \ "qngie"
+  val value = Some(Json.toJson(qngie))
+}
 
-  case class GroupEBITDADecimalError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "EBITDA_DECIMAL"
-    val errorMessage: String = "Group EBITDA must be to 2 decimal places or less"
-    val path: JsPath = topPath \ "groupEBITDA"
-    val value = Some(Json.toJson(groupEBITDA))
-  }
+case class GroupEBITDADecimalError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "EBITDA_DECIMAL"
+  val errorMessage: String = "Group EBITDA must be to 2 decimal places or less"
+  val path: JsPath = topPath \ "groupEBITDA"
+  val value = Some(Json.toJson(groupEBITDA))
+}
 
-  case class GroupRatioDecimalError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "GROUP_RATIO_DECIMAL"
-    val errorMessage: String = "Group ratio must be to 5 decimal places or less"
-    val path: JsPath = topPath \ "groupRatio"
-    val value = Some(Json.toJson(groupEBITDA))
-  }
+case class GroupRatioDecimalError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "GROUP_RATIO_DECIMAL"
+  val errorMessage: String = "Group ratio must be to 5 decimal places or less"
+  val path: JsPath = topPath \ "groupRatio"
+  val value = Some(Json.toJson(groupEBITDA))
+}
 
-  case class GroupRatioError(groupRatio: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "GROUP_RATIO_RANGE"
-    val errorMessage: String = "Group ratio must be between 0% and 100%"
-    val path: JsPath = topPath \ "groupRatio"
-    val value = Some(Json.toJson(groupRatio))
-  }
+case class GroupRatioError(groupRatio: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "GROUP_RATIO_RANGE"
+  val errorMessage: String = "Group ratio must be between 0% and 100%"
+  val path: JsPath = topPath \ "groupRatio"
+  val value = Some(Json.toJson(groupRatio))
+}
 
-  case class GroupRatioCalculationError(details: AdjustedGroupInterestModel)(implicit topPath: JsPath) extends Validation {
-    val code = "GROUP_RATIO_CALC"
-    val errorMessage: String = s"The value of the group ratio percentage must match the value calculated by dividing the QNGIE by the group EBITDA then multiplying it by 100"
-    val path: JsPath = topPath \ "groupEBITDA"
-    val value = Some(Json.toJson(details))
-  }
+case class GroupRatioCalculationError(details: AdjustedGroupInterestModel)(implicit topPath: JsPath) extends Validation {
+  val code = "GROUP_RATIO_CALC"
+  val errorMessage: String = s"The value of the group ratio percentage must match the value calculated by dividing the QNGIE by the group EBITDA then multiplying it by 100"
+  val path: JsPath = topPath \ "groupEBITDA"
+  val value = Some(Json.toJson(details))
+}
 
-  case class NegativeOrZeroGroupEBITDAError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "EBITDA_NEGATIVE"
-    val errorMessage: String = "If group EBITDA is negative or zero, set group ratio to 100"
-    val path: JsPath = topPath \ "groupEBITDA"
-    val value = Some(Json.toJson(groupEBITDA))
-  }
+case class NegativeOrZeroGroupEBITDAError(groupEBITDA: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "EBITDA_NEGATIVE"
+  val errorMessage: String = "If group EBITDA is negative or zero, set group ratio to 100"
+  val path: JsPath = topPath \ "groupEBITDA"
+  val value = Some(Json.toJson(groupEBITDA))
+}
 
-  case class NegativeOrZeroGroupRatioError(groupRatio: BigDecimal)(implicit topPath: JsPath) extends Validation {
-    val code = "GROUP_RATIO_NEGATIVE"
-    val errorMessage: String = "If group ratio calculation is negative or zero, then set group ratio to 100"
-    val path: JsPath = topPath \ "groupRatio"
-    val value = Some(Json.toJson(groupRatio))
-  }
+case class NegativeOrZeroGroupRatioError(groupRatio: BigDecimal)(implicit topPath: JsPath) extends Validation {
+  val code = "GROUP_RATIO_NEGATIVE"
+  val errorMessage: String = "If group ratio calculation is negative or zero, then set group ratio to 100"
+  val path: JsPath = topPath \ "groupRatio"
+  val value = Some(Json.toJson(groupRatio))
+}
