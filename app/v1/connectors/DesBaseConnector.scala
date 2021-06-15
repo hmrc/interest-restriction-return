376e536073f0fb19af6495ee2e7ebf2cbf1dfd97
DevAndyroo <andy@andyroo.info>
2021-06-15 10:41:46 +0100

    CIR-1749: Add correlationId to request

--- app/v1/connectors/DesBaseConnector.scala
+++ app/v1/connectors/DesBaseConnector.scala
@@ -24,18 +24,23 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.http.Authorization
 import v1.models.requests.IdentifierRequest
 
+import java.util.UUID
+
 trait DesBaseConnector {
 
   _: Logging =>
 
-  def desHc(implicit hc: HeaderCarrier, appConfig: AppConfig, request: IdentifierRequest[_]): HeaderCarrier =
-    hc.withExtraHeaders(appConfig.desEnvironmentHeader, "providerId" -> request.identifier)
+  def desHc(implicit hc: HeaderCarrier, appConfig: AppConfig, request: IdentifierRequest[_]): HeaderCarrier = {
+    val correlationId = UUID.randomUUID()
+    logger.debug(s"Prepping message with correlationId header: $correlationId")
+    hc.withExtraHeaders(appConfig.desEnvironmentHeader, "providerId" -> request.identifier, "correlationId" -> correlationId.toString)
       .copy(authorization = Some(Authorization(appConfig.desAuthorisationToken)))
+  }
 
   def handleHttpResponse(response: HttpResponse, parserName: String, unexpectedErrorMessage: String): SubmissionResponse = {
     response.status match {
       case CREATED =>
-        logger.info(s"Successfully created with response ${response}") // TODO correlation id /
+        logger.info(s"Successfully created with response ${response}")
         logger.debug(s"Status CREATED")
         logger.debug(s"Json Response: ${response.json}")
         response.json.validate[DesSuccessResponse](DesSuccessResponse.fmt).fold(
