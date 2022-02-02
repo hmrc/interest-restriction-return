bc3b1974bd67e7d0756e7eb29fb8dfa4955131ca
yangwong9664 <20066247+yangwong9664@users.noreply.github.com>
2022-02-02 16:01:47 +0000

    test

--- app/v1/models/authAction/ForbiddenIndividualsError.scala
+++ app/v1/models/authAction/ForbiddenIndividualsError.scala
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package v1.models.authAction
 
 import play.api.http.Status.FORBIDDEN
 import play.api.libs.json.{Json, OFormat}
 
-case class ForbiddenIndividualsError(status: Int = FORBIDDEN, message: String =
+case class ForbiddenIndividualsError(code: Int = FORBIDDEN, message: String =
 "You are unable to access this service as an individual. This service is only available to individual companies or groups of companies.")
 
 object ForbiddenIndividualsError {
