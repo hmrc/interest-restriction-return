a1b16002355bae3459999443cf23bfa34597099d
Victor Osipov <osipovvictor1994@gmail.com>
2025-06-20 13:25:10 +0100

    DDCE-6742 - migrate /return from DES to HIP

--- app/v1/connectors/AbbreviatedReturnConnector.scala
+++ app/v1/connectors/AbbreviatedReturnConnector.scala
@@ -16,9 +16,11 @@
 
 package v1.connectors
 
+import com.google.inject.Singleton
 import config.AppConfig
 import play.api.http.HeaderNames
 import play.api.libs.json.Json
+import play.api.libs.ws.writeableOf_JsValue
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
 import utils.JsonFormatters
@@ -26,11 +28,11 @@ import v1.connectors.HttpHelper.SubmissionResponse
 import v1.connectors.httpParsers.AbbreviatedReturnHttpParser.AbbreviatedReturnReads
 import v1.models.abbreviatedReturn.AbbreviatedReturnModel
 import v1.models.requests.IdentifierRequest
-import play.api.libs.ws.writeableOf_JsValue
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
 class AbbreviatedReturnConnector @Inject() (httpClient: HttpClientV2, implicit val appConfig: AppConfig)
     extends BaseConnector
     with JsonFormatters {
@@ -54,4 +56,23 @@ class AbbreviatedReturnConnector @Inject() (httpClient: HttpClientV2, implicit v
       .withBody(Json.toJson(abbreviatedReturnModel))
       .execute[SubmissionResponse]
   }
+
+  def submitAbbreviatedReturnHip(
+    abbreviatedReturnModel: AbbreviatedReturnModel
+  )(implicit hc: HeaderCarrier, ec: ExecutionContext, request: IdentifierRequest[?]): Future[SubmissionResponse] = {
+    val fullUrl      = appConfig.hipAbbreviatedReturnUrl
+    logger.debug(s"[AbbreviatedReturnConnector][submitAbbreviatedReturnHip] URL: $fullUrl")
+    val receivedSize = request.headers.get(HeaderNames.CONTENT_LENGTH)
+    val jsonSize     = Json.stringify(Json.toJson(abbreviatedReturnModel)).length
+    logger.debug(
+      s"[AbbreviatedReturnConnector][submitAbbreviatedReturnHip] Size of content received: $receivedSize sent: $jsonSize"
+    )
+
+    httpClient
+      .post(url"$fullUrl")
+      .setHeader(hipHeaders(appConfig)*)
+      .withBody(Json.toJson(abbreviatedReturnModel))
+      .execute[SubmissionResponse]
+  }
+
 }
